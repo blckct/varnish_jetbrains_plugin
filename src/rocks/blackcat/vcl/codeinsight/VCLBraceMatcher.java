8a0abb17c5bb6628de8b745d6208c85055e48b26
Jakub Kania <jakubkania@blackcat.rocks>
2017-10-31 01:36:47 +0100

    Add brace matching

---
+++ src/rocks/blackcat/vcl/codeinsight/VCLBraceMatcher.java
@@ -0,0 +1,31 @@
+package rocks.blackcat.vcl.codeinsight;
+
+import com.intellij.lang.BracePair;
+import com.intellij.lang.PairedBraceMatcher;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.tree.IElementType;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import rocks.blackcat.vcl.psi.VCLTypes;
+
+
+public class VCLBraceMatcher implements PairedBraceMatcher{
+
+    @NotNull
+    @Override
+    public BracePair[] getPairs() {
+        BracePair[] bp = {new BracePair(VCLTypes.LC, VCLTypes.RC, true),
+                        new BracePair(VCLTypes.LP, VCLTypes.RP, true)};
+        return bp;
+    }
+
+    @Override
+    public boolean isPairedBracesAllowedBeforeType(@NotNull IElementType iElementType, @Nullable IElementType iElementType1) {
+        return true;
+    }
+
+    @Override
+    public int getCodeConstructStart(PsiFile psiFile, int i) {
+        return i;
+    }
+}
