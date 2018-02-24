47e2c85b636617dffd44028efd3b212e3b70c1b6
Jakub Kania <jakubkania@blackcat.rocks>
2018-02-24 19:23:53 +0100

    Add some basic annotations for upgrading Varnish 3 to 4

--- src/rocks/blackcat/vcl/VCLAnnotator.java
+++ src/rocks/blackcat/vcl/VCLAnnotator.java
@@ -7,13 +7,11 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.*;
 import org.jetbrains.annotations.NotNull;
-import rocks.blackcat.vcl.psi.VCLFile;
-import rocks.blackcat.vcl.psi.VCLPurge;
-import rocks.blackcat.vcl.psi.VCLUnset;
+import rocks.blackcat.vcl.psi.*;
 
 import java.util.List;
 
-public class VCLAnnotator implements Annotator{
+public class VCLAnnotator implements Annotator {
     @Override
     public void annotate(@NotNull final PsiElement element, @NotNull AnnotationHolder holder) {
         // purge can be a keyword but it could also be a name of ACL list or some other object so highlight it manually
@@ -21,15 +19,29 @@ public class VCLAnnotator implements Annotator{
             Annotation annotation = holder.createInfoAnnotation(element.getTextRange(), null);
             annotation.setTextAttributes(DefaultLanguageHighlighterColors.KEYWORD);
         }
-        if (element instanceof VCLUnset){
-            VCLFile file = (VCLFile)element.getContainingFile();
-            if (file.getVersion() > 3.0){
-                //System.out.print(element.getFirstChild().getText());
-                if(element.getFirstChild().getText().equals("remove")){
-                    holder.createWarningAnnotation(element, "Remove keyword was removed in version 4, use unset");
-                    System.out.print("New Version");
+
+        //Warnings fo outdated keywords
+        if (isVarnish4(element)) {
+            if (element instanceof VCLUnset) {
+                if (element.getFirstChild().getText().equals("remove")) {
+                    holder.createErrorAnnotation(element, "Remove keyword was removed in version 4, use unset");
                 }
+            }
+
+            if (element instanceof VCLPurge) {
+                holder.createErrorAnnotation(element, "Purge keyword was removed in version 4, use  return(purge)");
+
+            }
+
+            if (element instanceof VCLError) {
+                holder.createErrorAnnotation(element, "Error keyword was removed in version 4, use  return (synth(999, \"Response\"))");
             }
         }
     }
+
+    private boolean isVarnish4(@NotNull final PsiElement element) {
+        VCLFile file = (VCLFile) element.getContainingFile();
+        return file.getVersion() > 3.0;
+    }
+
 }
